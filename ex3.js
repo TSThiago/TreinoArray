759e9c8503a4216387a9a4169a963d51a6d20f31
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-24 21:14:26 -0300

    Criação do programa ex3 e criação de 2 vetores.

---
+++ ex3.js
@@ -0,0 +1,8 @@
+// Crie um algoritmo com dois vetores e os preencha com números inteiros e
+// positivos, o usuário deve dizer quando não quer mais inserir valores mas os vetores
+// devem ter a mesma quantidade de números. Depois crie um terceiro vetor que irá
+// receber apenas os números que estão em ambos os vetores anteriores ou forem
+// ímpar.
+// Ex.: entrada - arrayA[2,3,4,8,1] arrayB[7,4,1,6,8]
+// saída - arrayC[3,4,8,1]
+
